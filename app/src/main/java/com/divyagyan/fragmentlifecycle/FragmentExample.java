a6bb54bb013cd635391ac78f96805785e90550a6
Pradesh Shrestha <pradeshshrestha911@gmail.com>
2023-04-16 09:21:22 +0545

    intial commit

---
+++ app/src/main/java/com/divyagyan/fragmentlifecycle/FragmentExample.java
@@ -0,0 +1,19 @@
+package com.divyagyan.fragmentlifecycle;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentExample extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       View view = inflater.inflate(R.layout.fragment_example, container, false);
+        return view;
+    }
+}
